48dc04004a2746a1067cee492cda122bfd1e9912
Liquid Monks <info@liquidmonks.com>
2022-11-22 13:29:39 -0600

    script.js includes global variable lists that grab ID elements from the header and main section of index.html.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/* Global Variables */
+
+// Grab #btnSearch click event when user enters a value in the search field and presses the enter key or clicks the search button
+let btn = document.querySelector("#btnSearch");
+
+// Grab the #pastCity click event when user clicks on a city in the past city list
+let pastCity = document.querySelector("#pastCity");
+
+// Grab the #searchCity input field to get the value of the city the user entered
+let searchCity = document.querySelector("#searchCity");
+
+// Grab the #forecastCity div to display the city name and date
+let forecastCity = document.querySelector("#forecastCity");
