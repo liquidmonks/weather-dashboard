bcdc3af2ad6f656606b1445e2184f94128233fbd
Liquid Monks <info@liquidmonks.com>
2022-11-22 13:55:51 -0600

    script.js includes page load initialization functions and listener click event for the search button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/* Global Variables */
+/*********************** Global Variables ***********************/
 
 // Grab #btnSearch click event when user enters a value in the search field and presses the enter key or clicks the search button
 let btn = document.querySelector("#btnSearch");
@@ -11,3 +11,32 @@ let searchCity = document.querySelector("#searchCity");
 
 // Grab the #forecastCity div to display the city name and date
 let forecastCity = document.querySelector("#forecastCity");
+
+/*********** Execute Functions **********/
+/*                                      */
+// Function to initialize the page
+/*                                      */
+init();
+/*                                      */
+// Listen for click event on #btnSearch
+/*                                      */
+btn.addEventListener("click", search);
+/*                                      */
+/****************************************/
+
+// TODO: Container to hold and parse the city name and date from local storage
+
+/******************************************* Functions ***********************************************/
+/*                                                                                                   */
+// TODO: Function to get the current weather for the city the user entered
+/*                                                                                                   */
+/*                                                                                                   */
+/*                                                                                                   */
+// TODO: Initialize the function to get the current weather for the city the user entered
+/*                                                                                                   */
+/*                                                                                                   */
+/*                                                                                                   */
+// TODO: Function to grab data from local storage and display it in the past city list
+/*                                                                                                   */
+/*                                                                                                   */
+/*****************************************************************************************************/
