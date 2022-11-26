ac4bc9ef10988f2e0ee9aa96d97821c1176e881c
Liquid Monks <info@liquidmonks.com>
2022-11-26 14:02:28 -0600

    script.js includes revisions to global variable set that better ties the values to the search function block.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,17 @@
 /*********************** Global Variables ***********************/
 
 // Grab #btnSearch click event when user enters a value in the search field and presses the enter key or clicks the search button
-let btn = document.querySelector("#btnSearch");
+let btn = $("#btnSearch");
 
 // Grab the #pastCity click event when user clicks on a city in the past city list
-let pastCity = document.querySelector("#pastCity");
+let pastCity = $("#pastCity");
 
 // Grab the #searchCity input field to get the value of the city the user entered
-let searchCity = document.querySelector("#searchCity");
-
-// Grab the #forecastCity div to display the city name and date
-let forecastCity = document.querySelector("#forecastCity");
+let searchCity = $("#searchCity");
 
 /*********** Execute Functions **********/
 /*                                      */
-// Function to initialize the page
-/*                                      */
-init();
-/*                                      */
-/*                                      */
+
 function init() {
   loadCity();
 }
